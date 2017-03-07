f75ac4d43b1724e99c1e536128ad76aa519de653
Thanh Nguyen <nqthanh@tma.com.vn>
2017-03-07 16:44:03 +0700

    Update js

---
+++ Ass/js/myJS.js
@@ -0,0 +1,31 @@
+function setTimeCounter(countDownDate, id) {
+  var x = setInterval(() => {
+
+    // Get todays date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now an the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="demo"
+    document.getElementById(id).innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+
+    // If the count down is finished, write some text
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById(id).innerHTML = "EXPIRED";
+    }
+  }, 1000);
+}
+
+$(".side-grid-sidebar-more").hover((e) => {
+  $(".side-grid-content").css('display', 'none');
+  $("#side-grid-" +e.target.id.slice(-1)).css('display', "block");
+});
